f6e71a2d2765d35d6efdb77f76fcca75382ec3ed
Iara Pou <iarapou21@hotmail.com>
2022-11-17 17:30:04 -0300

    Ordena código

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+function crearContenedor() {
+    let divNuevo = document.createElement("div");
+    divNuevo.classList.add("item");
+}
+
+function crearInput(nuevaTarea) {
+    let inputItem = document.createElement("input");
+
+    inputItem.disabled = "true";
+    inputItem.classList.add("item-input");
+    inputItem.value = nuevaTarea;
+}
+
 function crearBotonEditar() {
     let botonEditar = document.createElement("button");
     botonEditar.innerHTML = "<i class='fas fa-lock'></i>";
@@ -11,30 +24,12 @@ function crearBotonRemover() {
     botonRemover.classList.add("boton-remover");
     botonRemover.onclick = eliminarContenedorTarea;
 }
-
-botonEditar.addEventListener("click", function () {
-    if (inputItem.disabled === true) {
-        //cambiar las clases con classList y no con innerHTML
-        botonEditar.innerHTML = "<i class='fas fa-lock-open'></i></i>";
-        inputItem.disabled = false;
-    } else {
-        botonEditar.innerHTML = "<i class='fas fa-lock'></i></i>";
-        inputItem.disabled = true;
-    }
-})
 function eliminarContenedorTarea(event){
     const $contenedor = event.target.parentNode;
     $contenedor.remove()
 }
 
-
-function crearContenedor() {
-    let divNuevo = document.createElement("div");
-    divNuevo.classList.add("item");
-}
-
 function agregar(nuevaTarea) {
-
     const $contenedorTareaNueva = crearContenedor();
     const $input = crearInput(nuevaTarea);
     const $botonEditar = crearBotonEditar();
@@ -44,18 +39,12 @@ function agregar(nuevaTarea) {
     $contenedorTareaNueva.appendChild($botonEditar);
     $contenedorTareaNueva.appendChild($botonRemover);
 
-    let $contenedorTareas = document.querySelector("#contenedor-tareas");
+    const $contenedorTareas = document.querySelector("#contenedor-tareas");
     $contenedorTareas.appendChild($contenedorTareaNueva);
 
 }
 
-function crearInput(nuevaTarea) {
-    let inputItem = document.createElement("input");
 
-    inputItem.disabled = "true";
-    inputItem.classList.add("item-input");
-    inputItem.value = nuevaTarea;
-}
 
 function validarInput() {
     if ($tareaNueva.value === "") {
